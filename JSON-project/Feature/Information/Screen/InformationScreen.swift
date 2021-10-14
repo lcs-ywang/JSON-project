912008ec43bc8116ee1cf14e6f793cfe0e7dc708
yining wang <ywang@lcs.on.ca>
2021-10-13 20:49:35 -0400

    using dummy data to examine the code

---
+++ JSON-project/Feature/Information/Screen/InformationScreen.swift
@@ -0,0 +1,50 @@
+//
+//  InformationScreen.swift
+//  JSON-project
+//
+//  Created by Yining Wang on 2021-10-13.
+//
+
+import SwiftUI
+
+struct InformationScreen: View {
+    var body: some View {
+        List{
+            ForEach(Detail.dummydata, id: \.firstname){ item in
+                VStack(alignment: .leading, spacing: 8) {
+                    
+                    HStack{
+                        Image(systemName: "tv")
+                            .font(.system(size: 12, weight: .black))
+                        Text(item.firstname)
+                    }
+                    Text(makeAttributedString(title:"username", label: item.username))
+                    Text(makeAttributedString(title:"Lastname", label: item.lastname))
+                    lineLimit(2)
+                }
+                
+                .padding()
+                .foregroundColor(.black)
+                
+            }
+        }
+   
+    }
+    private func makeAttributedString(title: String, label: String) -> AttributedString{
+        var string = AttributedString("\(title): \(label)")
+        string.foregroundColor = .black
+        string.font = .system(size: 16, weight: .bold)
+        
+        if let range = string.range(of: label) {
+            string[range].foregroundColor = .black.opacity(0.8)
+            string[range].font = .system(size: 16, weight: .regular)
+        }
+        return string
+    }
+}
+
+struct InformationScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        InformationScreen()
+    }
+}
