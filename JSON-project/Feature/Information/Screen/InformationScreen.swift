58d10c0aa2a6d457ac23aaa879e56c1a3d879220
yining wang <ywang@lcs.on.ca>
2021-10-13 21:10:57 -0400

    Put the screen interface into the view

--- JSON-project/Feature/Information/Screen/InformationScreen.swift
+++ JSON-project/Feature/Information/Screen/InformationScreen.swift
@@ -11,36 +11,14 @@ struct InformationScreen: View {
     var body: some View {
         List{
             ForEach(Detail.dummydata, id: \.firstname){ item in
-                VStack(alignment: .leading, spacing: 8) {
-                    
-                    HStack{
-                        Image(systemName: "tv")
-                            .font(.system(size: 12, weight: .black))
-                        Text(item.firstname)
-                    }
-                    Text(makeAttributedString(title:"username", label: item.username))
-                    Text(makeAttributedString(title:"Lastname", label: item.lastname))
-                    lineLimit(2)
-                }
-                
-                .padding()
-                .foregroundColor(.black)
-                
+             
+                Detailview(item: item)
+            
             }
         }
    
     }
-    private func makeAttributedString(title: String, label: String) -> AttributedString{
-        var string = AttributedString("\(title): \(label)")
-        string.foregroundColor = .black
-        string.font = .system(size: 16, weight: .bold)
-        
-        if let range = string.range(of: label) {
-            string[range].foregroundColor = .black.opacity(0.8)
-            string[range].font = .system(size: 16, weight: .regular)
-        }
-        return string
-    }
+    
 }
 
 struct InformationScreen_Previews: PreviewProvider {
